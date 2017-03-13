02a50bc19e1fc8919f4206b7293b41abfdda2922
mdjsjdqe <mdjsjdqe@users.noreply.github.com>
2017-03-13 22:52:15 +0800

    开始使用github来管理我的代码
    这是全部的R语言相关的文件

---
+++ age and weight.R
@@ -0,0 +1,17 @@
+# input data
+age <-c(3,12,9,3,9,11,2,5,3,1)
+weight <-c(6.1,10.2,10.4,6,7.3,8.5,5.2,7.2,5.3,4.4)
+
+# Mean Analysis
+mean(age)
+mean(weight)
+
+# Standard Deviation
+sd(age)
+sd(weight)
+
+# Correlation Analysis
+cor(age,weight)
+
+# Draw plot
+plot(age,weight)
---
+++ package install and demo.R
@@ -0,0 +1,15 @@
+#设置工作目录
+setwd("/Users/SuperTux/Desktop/统计数据/R data/script learning 03.13")
+options()
+
+# 安装R包
+install.packages("gclus")
+install.packages("ggplot2")
+update.packages()
+
+# Demo 包的用法
+## 1. 摁下return查看下一个图
+demo(graphics)
+## 1. 下面的包没有演示
+demo(gclus)
+demo(ggplot2)
